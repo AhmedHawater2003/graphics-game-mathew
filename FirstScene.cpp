5b7d861f4d92415345fbc80c91f8adea2c5a1a89
Tarek Zeyad <tarekzeyad31@gmail.com>
2023-12-09 15:22:30 +0200

    Auto stash before merge of "master" and "feat/adding-obstecle"

--- FirstScene.cpp
+++ FirstScene.cpp
@@ -98,4 +98,62 @@ void FirstScene::setupLights()
 	
 	//glClearColor(127.0 / 255.0 * p, 207.0 / 255.0 * p, 255.0 / 255.0 * p, 0.0f);
 
+	GLfloat light_position[] = { 0.0, 100.0, 0.0, 1.0 };
+	GLfloat light_ambient[] = { p, p, p, 0 };
+	GLfloat light_diffuse[] = { 127.0 / 255.0 * p, 207.0 / 255.0 * p, 255.0 / 255.0 * p, 0.0f };
+	GLfloat light_specular[] = { p, p, p, 0.0f };
+	GLfloat shininess[] = { 0.5 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SHININESS, shininess);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+	glEnable(GL_LIGHT0);
+
+
+	Player* player = getGameObjectByTag<Player>("player");
+
+	// Spotlight
+	GLfloat light_position1[] = {
+		player->getPosition().getX(),
+		player->getPosition().getY() + 25,
+		player->getPosition().getZ() + 20,
+		1.0};
+	GLfloat light_ambient1[] = { 0.1, 0.1, 0.1, 0.0 };
+	GLfloat light_diffuse1[] = { 242.0 / 255.0, 235.0 / 255.0, 104.0 / 255.0, 0.0 }; // Increase diffuse intensity
+	GLfloat light_specular1[] = { 0.2, 0.2, 0.2, 0.0 }; // Increase specular intensity
+
+	GLfloat spot_direction[] = { 0.0, -2.0, 1 };
+
+	// Set spotlight properties
+	glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient1);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse1);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular1);
+	glLightfv(GL_LIGHT1, GL_SHININESS, shininess);
+
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 30.0);
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction);
+	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 1);
+
+	// Enable lighting and light1
+	glEnable(GL_LIGHT1);
+}
+
+void FirstScene::onTimer(int value)
+{
+	GameScene::onTimer(value);
+
+	hourOfDay += timeDirection;
+	if (hourOfDay == 24) {
+		timeDirection = -1;
+	}
+	else if (hourOfDay == 0) {
+		timeDirection = 1;
+	}
+}
+
+
 
