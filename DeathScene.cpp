6333438cc05cc9edd3fa1ede1b723bcca46eec71
Ahmed Hawater <hawatercoding@gmail.com>
2024-12-06 22:42:40 +0200

    Fix win scene

--- DeathScene.cpp
+++ DeathScene.cpp
@@ -12,6 +12,7 @@
 DeathScene::DeathScene()
 {
 	gameObjects["gameText"] = (new GameText());
+	gameObjects["camera"] = (new Camera({ 0, 20, -105 }, { 0, 0, 0 }, { 0, 1, 0 }, 1280.0f / 720));
 
 }
 
@@ -22,7 +23,7 @@ void DeathScene::onIdle()
 	GameText* gameText = getGameObjectByTag<GameText>("gameText");
 
 
-	gameText->setPosition({ -0.3, 0, 0 });
+	gameText->setPosition({ 30, 0, 0 });
 	gameText->setText("YOU DID IT, ADVENTURER!");
 
 }
